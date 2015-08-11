c58e227d9476b6ecccecf1db9b08ccc6c219f9a8
aweesomo <sam.joseph.miele@gmail.com>
2015-08-11 16:56:12 +0100

    check if on remote connection before pointlessly remapping keys

--- zsh/keybindings.zsh
+++ zsh/keybindings.zsh
@@ -1,8 +1,13 @@
 bindkey -v
 
-#backwards search
-bindkey '^r' history-incremental-search-backward
-bindkey "^[." insert-last-word
+if [ -z $SSH_CONNECTION ]; then
+    xmodmap -e 'keycode 135 = Control_L'
+    xmodmap -e 'keycode 108 = Alt_L'
+fi
 
+#backwards search
 bindkey -M isearch '^k' history-incremental-search-backward
 bindkey -M isearch '^j' history-incremental-search-forward
+bindkey '^r' history-incremental-search-backward
+
+bindkey "^[." insert-last-word
