9badc2e4a019a95fb97cefa748f1b8092a09f662
aweesomo <sam.joseph.miele@gmail.com>
2015-08-12 17:14:12 +0100

    ctrl-p

--- zsh/keybindings.zsh
+++ zsh/keybindings.zsh
@@ -11,3 +11,19 @@ bindkey -M isearch '^j' history-incremental-search-forward
 bindkey '^r' history-incremental-search-backward
 
 bindkey "^[." insert-last-word
+
+
+
+# Kudos to again to...yes Matthew 'that cntrl-p guy' Franglen.
+function vim-ctrlp() {
+
+    #if there's anything else on the buffer, then whack it on the stack
+    if [ ${#BUFFER} -ne 0 ]; then
+        zle push-line
+    fi
+    BUFFER='vim +:CtrlP'
+    zle accept-line
+}
+
+zle -N vim-ctrlp
+bindkey -M viins '^P' vim-ctrlp
